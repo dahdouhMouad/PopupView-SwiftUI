31a73d5fbe3acc8fffb54b86218775bfce4b72da
dahdouhMouad <mouaddhd10@gmail.com>
2023-11-20 16:04:59 +0100

    popup view idea

---
+++ PopUpSwiftUI/PopUp/PopupView.swift
@@ -0,0 +1,20 @@
+//
+//  PopupView.swift
+//  PopUpSwiftUI
+//
+//  Created by Macbook Pro on 20/11/2023.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopupView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopupView()
+    }
+}
