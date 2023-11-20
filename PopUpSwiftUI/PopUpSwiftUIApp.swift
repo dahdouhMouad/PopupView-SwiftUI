88570479e3a0afd5072df9eb5e9c3ee0149c5b8d
dahdouhMouad <mouaddhd10@gmail.com>
2023-11-20 14:57:54 +0100

    Initial Commit

---
+++ PopUpSwiftUI/PopUpSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpSwiftUIApp.swift
+//  PopUpSwiftUI
+//
+//  Created by Macbook Pro on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PopUpSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
